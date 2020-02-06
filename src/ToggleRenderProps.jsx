6fc9ef5134d08f5e9ca943ab12e42b715a0436b1
Dawit <dawit@ginetta.net>
2020-02-06 11:39:02 +0100

    Create ToggleRenderProps component

---
+++ src/ToggleRenderProps.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ToggleRenderProps extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
